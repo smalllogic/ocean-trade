e3c61698475c45b8918adcb75f1fa7ac4930ca9f
石斌 <inselfteaching@gmail.com>
2026-03-14 10:29:07 +0800

    first commit

--- db/migrate/20260313131820_remove_product_type_and_price_from_products.rb
+++ db/migrate/20260313131820_remove_product_type_and_price_from_products.rb
@@ -1,4 +1,4 @@
-class RemoveProductTypeAndPriceFromProducts < ActiveRecord::Migration[8.0]
+rails class RemoveProductTypeAndPriceFromProducts < ActiveRecord::Migration[8.0]
   def change
     remove_column :products, :product_type, :string
     remove_column :products, :price, :decimal
